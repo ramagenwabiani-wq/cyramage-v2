5c3ebb8d713984a58433d67212668499bac6054c
ramagenwabiani-wq <ramage.nwabiani@gmail.com>
2026-07-27 19:15:55 +0100

    Remove changeImage function
    Remove changeImage function that updates main image and thumbnails.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,1 @@
-function changeImage(img) {
-    const mainImage = document.getElementById("mainImage");
 
-    if (mainImage) {
-        mainImage.src = img.src;
-    }
-
-    document.querySelectorAll(".thumbnail").forEach(t => {
-        t.classList.remove("active");
-    });
-
-    img.classList.add("active");
-}
